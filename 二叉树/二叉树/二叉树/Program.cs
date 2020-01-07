dc1b62028012b994d5678ec0253dee1aa37ca86e
liuyixin1125 <58197205+liuyixin1125@users.noreply.github.com>
2020-01-07 11:06:30 +0800

    二叉树
    二叉树

---
+++ 二叉树/二叉树/二叉树/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 二叉树
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0;
+            double qian = 0;
+            for (int i = 1; qian <= 10000; i++)
+            {
+                qian += ((Math.Pow(2, i)) * 10000) * (Math.Pow(0.15, i));
+                a = i;
+            }
+            Console.WriteLine("发展层数：" + a);
+        }
+    }
+}
